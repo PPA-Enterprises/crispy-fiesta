b3412ea7e993c3eaeea7abd52d402df8293be3cf
Joshua Benz <joshua@planethull.com>
2020-06-27 19:51:06 -0700

    responses and notes

--- controllers/job.go
+++ controllers/job.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"net/http"
 	"github.com/gin-gonic/gin"
 	"github.com/PPA-Enterprises/crispy-fiesta/forms"
 	"github.com/PPA-Enterprises/crispy-fiesta/models"
@@ -12,11 +13,18 @@ type JobController struct {}
 func (j *JobController) Create(c *gin.Context) {
 	var data forms.SubmitJobCmd
 	if c.BindJSON(&data) != nil {
-		c.JSON(406, gin.H{"message": "Provide relevant fields"})
-		c.Abort()
+		c.JSON(http.StatusNotAcceptable, gin.H{"message": "Provide relevant fields"});
+		c.Abort();
 		return
 	}
-	job := models.FromSubmitJobCmd(data)
-	id, _ := job.PersistJob()
-	c.JSON(201, gin.H{"message": id.String()})
+	job := models.FromSubmitJobCmd(data);
+	id, err := job.PersistJob();
+
+	if err != nil {
+		//TODO: check for err type and respond accordingly
+		return c.JSON(http.StatusCreated,
+			gin.H{"success": false, "payload": "", "message": "Job Create Failed"});
+	}
+	return c.JSON(http.StatusCreated,
+		gin.H{"success": true, "payload": id.String(), "message": "Job Created"});
 }
