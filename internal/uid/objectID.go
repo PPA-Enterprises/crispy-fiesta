44680094464cc7c64e9f30f281aa685cb692de27
Joshua Benz <joshua@planethull.com>
2020-07-24 00:20:56 -0700

    change  ID to use pointers

--- internal/uid/objectID.go
+++ internal/uid/objectID.go
@@ -13,17 +13,17 @@ type uid struct {
 	objectID primitive.ObjectID
 }
 
-func (self uid) String() string {
+func (self *uid) String() string {
 	return self.objectID.Hex()
 }
 
 func TryFromInterface(id interface{}) (ID, *errors.ResponseError) {
 	if res, ok := id.(primitive.ObjectID); ok {
-		return uid{objectID: res}, nil
+		return &uid{objectID: res}, nil
 	}
 	return nil, errors.UidTypeAssertionError()
 }
 
 func FromOid(oid primitive.ObjectID) ID {
-	return uid{objectID: oid}
+	return &uid{objectID: oid}
 }
