87a11d4a7c08106beb0978f5a775af0c055cb114
Joshua Benz <joshua@planethull.com>
2021-01-15 23:22:05 -0800

    token and secure

---
+++ backend/pkg/common/secure/secure.go
@@ -0,0 +1,34 @@
+package secure
+
+import (
+	"fmt"
+	"hash"
+	"strconv"
+	"time"
+	"github.com/matthewhartstonge/argon2"
+
+)
+
+// New initializes security service
+func New() *Service {
+	cfg := argon2.DefaultConfig()
+	return &Service{cfg: cfg}
+}
+
+// Service holds security related methods
+type Service struct {
+	cfg		argon2.Config
+}
+
+
+// Hash hashes the password using bcrypt
+func (self *Service) Hash(password string) string {
+	raw, _ := self.cfg.Hash([]byte(password), nil)
+	return string(raw.Encode())
+}
+
+// HashMatchesPassword matches hash with password. Returns true if hash and password match.
+func (*Service) HashMatchesPassword(hash, password string) bool {
+	res, _ := argon2.VerifyEncoded([]byte(password), []byte(hash))
+	return res
+}
