d20459b38da44f110512eb0be9389001644ad532
Joshua Benz <joshua@planethull.com>
2021-01-22 12:56:47 -0800

    token structs

--- backend/auth.go
+++ backend/auth.go
@@ -1,5 +1,12 @@
 package PPA
 
-type AuthToken struct {}
-type RefreshToken struct {}
+type AuthToken struct {
+	Token string `json:"token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type RefreshToken struct {
+	Token string `json:"token"`
+}
+
 type RBAC interface {}
