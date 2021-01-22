deddf26be675dd0e99f1bedc5f31aec6d31cd48b
Joshua Benz <joshua@planethull.com>
2021-01-22 12:52:30 -0800

    added roles

---
+++ backend/role.go
@@ -0,0 +1,15 @@
+package PPA
+
+type AccessRole int
+
+const(
+	SuperAdminRole AccessRole = 100
+	AdminRole AccessRole = 110
+	UserRole AccessRole = 200
+)
+
+type Role struct {
+	ID AccessRole `json:"id"`
+	AccessLevel AccessRole `json:"access_level"`
+	Name string `json:"name"`
+}
