66e4a4a01416ae509a747f60c02b3167b8b9ec24
Joshua Benz <joshua@planethull.com>
2021-01-26 20:45:26 -0800

    job service interfaces

---
+++ backend/pkg/api/job/service.go
@@ -0,0 +1,33 @@
+package job
+import(
+	"PPA"
+	"context"
+	"pkg/common/mongo"
+	dbQuery "pkg/api/job/infra/mongo"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/gin-gonic/gin"
+)
+
+type Job struct {
+	db *mongo.DBConnection
+	jdb Repository
+	rbac RBAC
+}
+
+type Service interface {
+	Create(*gin.Context, PPA.Job) (*PPA.Job, error)
+	List(*gin.Context) (*[]PPA.Job, error)
+	ViewById(*gin.Context, string) (*PPA.Job, error)
+	Delete(*gin.Context, string) error
+	Update(*gin.Context, Update, string) (*PPA.Client, error)
+}
+
+type Repository interface {
+	Create(*mongo.DBConnection, context.Context, *PPA.Job) (*PPA.Job, error)
+	List(*mongo.DBConnection, context.Context) (*[]PPA.Job, error)
+	ViewById(*mongo.DBConnection, context.Context, primitive.ObjectID) (*PPA.Job, error)
+	Delete(*mongo.DBConnection, context.Context, primitive.ObjectID) (*PPA.Job, error)
+	Update(*mongo.DBConnection, context.Context, primitive.ObjectID, *PPA.Job) error
+}
+
+type RBAC interface{}
