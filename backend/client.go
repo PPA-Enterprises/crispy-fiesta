5018eda061dbafef40bee8a536f84c190c2103e9
Joshua benz <joshua@planethull.com>
2021-03-22 22:16:29 -0700

    move label to client since it is a client label

--- backend/client.go
+++ backend/client.go
@@ -32,3 +32,14 @@ func (c *Client) FindAndRemoveJob(jobOid primitive.ObjectID) {
 func (c *Client) AppendLog(event LogEvent) {
 	c.History = append(c.History, event)
 }
+
+type Label struct {
+	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	LabelName string `json:"label_name" bson:"label_name,omitempty"`
+	Clients []primitive.ObjectID `json:"clients" bson:"clients,omitempty"`
+	History []LogEvent `json:"history" bson:"history,omitempty"`
+}
+
+func(l *Label) AppendLog(event LogEvent) {
+	l.History = append(l.History, event)
+}
